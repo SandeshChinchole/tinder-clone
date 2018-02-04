2f22827a88e02984d5b74a59b317bc43e18d2264
Sandeep Singh <wwaheguru9509088985@gmail.com>
2018-02-04 15:29:14 +0530

    commit first to hasura from team

---
+++ microservices/ui/app/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './public',
+    compress: true,
+    port: 8080
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules:
+    [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+ }
+};
