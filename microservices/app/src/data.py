c2b37b56d0c6df5976e288123649f407c3068d07
Sandeep Singh <wwaheguru9509088985@gmail.com>
2018-02-24 00:47:37 +0530

    f

--- microservices/app/src/data.py
+++ microservices/app/src/data.py
@@ -39,26 +39,32 @@ def get_articles():
 
 
 @app.route("/get-allusers-info")
-def get():
-    if ('hasura-app.io' in request.url_root) or \
-       ('data.hasura' not in dataUrl):
+def getalluserinfo():
 
-        query = {
-            "type": "select",
-            "args": {
-                "table": "article",
-                "columns": [ "title", "id", "author_id", "rating", "title" ],
-                "limit": 10
-            }
-        }
-
-        response = requests.post(dataUrl, data=json.dumps(query))
-        if response.status_code == 200:
-            return render_template('data.html', data=json.dumps(response.json(), indent=2, sort_keys=True))
-        else:
-            return 'Something went wrong: <br/>' + str(response.status_code) + '<br/>' + response.text
-
-    # Change the data URL during local development
-    return ("""Edit the dataUrl variable in
-        <code>microservices/app/src/hasura.py</code>
-        to test locally.""")
+if request.headers['X-Hasura-User-Id'] != 1:
+# This is the url to which the query is made
+# Setting headers
+    headers = {
+          "Content-Type": "application/json",
+          'X-Hasura-User-Id': 1,
+          'X-Hasura-Role': "admin",
+          "X-Hasura-Allowed-Roles": "user,admin"
+    }
+    query = {
+    'type': 'select',
+    'args': {
+    'table': 'userinfo',
+    'columns': [
+      '*'
+        ]
+      }
+    }
+    print(url)
+    print(json.dumps(query))
+    response = requests.post(
+        dataUrl, data=json.dumps(query),headers=headers
+    )
+    data = response.json()
+    print(json.dumps(data))
+    return jsonify(data=data)
+else: return jsonify(message = "get-allusers-info request failed")
